d6f0529f650419d32a832dd6df2e138bfd11e4fa
jamey.weyenberg <jamey.weyenberg@cardinalhealth.com>
2023-08-24 14:42:07 -0400

    End of interview Aug 24 2023

--- src/test/java/com/cah/pace/anchorengineerkata/AnchorEngineerVendingMachineTests.java
+++ src/test/java/com/cah/pace/anchorengineerkata/AnchorEngineerVendingMachineTests.java
@@ -6,9 +6,38 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AnchorEngineerVendingMachineTests {
+
+    double[][] quarter = {{0.955}, {5.67}};
+
     @Test
-    void workingAssert() {
-        Assertions.assertTrue(true);
+    void testCoinsAreStored() {
+        //something to store the coins
+        VendingMachine vendingMachine = new VendingMachine();
+        double coinValue = 0.25;
+        vendingMachine.acceptCoins(quarter);
+        Assertions.assertEquals(coinValue, vendingMachine.getValueOfCoins());
     }
 
+    private class VendingMachine{
+        private double valueOfCoins = 0;
+
+        public void acceptCoins(double[][] coin){
+//            valueOfCoins += coin;
+            if (isValidCoin(coin)){
+//                valueOfCoins += coin;
+            }
+
+        }
+
+        private boolean isValidCoin(double[][] coin){
+            return false;
+        }
+
+        public double getValueOfCoins(){
+            return valueOfCoins;
+        }
+
+    }
 }
+
+
